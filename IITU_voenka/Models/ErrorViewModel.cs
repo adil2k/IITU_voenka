d164cce42eeb0d9c236aaa1e29bbd80b76dd9bd8
Adil <kaliev.adil99@gmail.com>
2020-11-22 23:21:26 +0600

    Добавьте файлы проекта.

---
+++ IITU_voenka/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IITU_voenka.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
